a781495611d2c3ea2d89da38f18af5ad91731f4a
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-08-30 16:06:29 -0500

    Run the compiled C with passed in argument
    Added functionality to run the compiled C code from the extracted archive, using
    a newly added command line argument representing the index for the prime.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Rename the first command line  argument into 'primeIndex'
+primeIndex="$1"
+
 # Extract the contents of the 'NthPrime.tgz' file into the working directory
 tar -xf NthPrime.tgz
 
 # Compiles the source .c file into the desired output file
 gcc ./NthPrime/*.c -o NthPrime/NthPrime
+
+# Run the newly created NthPrime/NthPrime.out file with the argument passed into this script
+./NthPrime/NthPrime "$primeIndex"
