dec88b6c906105bd60d656d3a28ccccfaed7e269
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-08-30 15:54:28 -0500

    Compile C output extracted by the `tar` command
    The added command will compile all of the `.c` files in the `NthPrime/`
    directory and then pipes the (in this case tested to be one) output file into
    NthPrime.out, residing in the NthPrime/ directory.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -3,3 +3,5 @@
 # Extract the contents of the 'NthPrime.tgz' file into the working directory
 tar -xf NthPrime.tgz
 
+# Compiles the source .c file into the desired output file
+gcc ./NthPrime/*.c -o NthPrime/NthPrime
