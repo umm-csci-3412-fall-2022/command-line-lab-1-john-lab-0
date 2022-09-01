9003ad8bbf74e1bd6697ca5666a0b14788368307
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-01 14:53:08 -0500

    Add  extraction to scratch directory
    Added functionality to the `big_clean.sh` file to take an input parameter,
    and then extract the input archive into a newly created scratch directory.
    As a temporary addition, the script also displays the contents of the scratch directory to verify functionality.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# Rename the command line parameter to 'inputFile'
+inputFile="$1"
+
+# Make a temporary directory to not interfere with existing and unrelated file structures
+tempDir=$(mktemp -d)
+
+# Extract the contents of the input archive file into the temporary directory
+tar -xf "$inputFile" --directory "$tempDir"
+
+# For testing purposes, navigate to and  display the contents of the temporary directory
+cd "$tempDir"
+ls -R
+
+# Remove the temporary directory after completion
+rm -rf "$tempDir"
