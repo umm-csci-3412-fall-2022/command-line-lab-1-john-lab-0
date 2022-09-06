e1e740664c87643e5b4eeae0de8ca1684843ac7a
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2022-09-06 15:11:23 -0500

    Fix removing the temporary extracted directory
    Fixed the failing test for the directory removal after copying into the current directory.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -25,19 +25,16 @@ done
 # Get the base file name (without extenxtion) of the input archive file
 inputFileNameWithoutExtension=$(basename "$inputFile" .tgz)
 
-# Display the inputFileNameWithoutExtension for verification
-echo "$inputFileNameWithoutExtension"
-
 # Find the parent directory for the files we want
 # (which are going to be buried in a bunch of temp folders)
 # Based on the approved answer from this stackExchange post
 # https://unix.stackexchange.com/questions/103528/how-do-i-search-all-subdirectories-to-find-one-with-a-certain-name
 contentPath=$(find "$tempDir" -type d -name "${inputFileNameWithoutExtension}")
 
-# Move the output files from the temporary directory into the directory ./uncompressedOutput
-mv -f "$contentPath" ./"$inputFileNameWithoutExtenstion"
+# Move the output files from the temporary directory into the directory
+mv -v "$contentPath" "$inputFileNameWithoutExtension"
 # Create a new archive file with the remaining files.
 tar -zcf  "cleaned_${inputFile}" "$inputFileNameWithoutExtension"
 
 # Remove the temporary folders
-rm -rf "$inputFileNameWithoutExtenstion"
+rm -rv "$inputFileNameWithoutExtension"
